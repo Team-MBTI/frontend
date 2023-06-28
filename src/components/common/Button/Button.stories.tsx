32b22bbb45754cf98c7b9b11e3f41b9d946f6d8c
sungjin.kim@voithru.com <sungjin.kim@voithru.com>
2023-06-29 01:30:10 +0900

    feat: button component ing..

--- src/components/common/Button/Button.stories.tsx
+++ src/components/common/Button/Button.stories.tsx
@@ -3,11 +3,75 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Button from './index';
 
 export default {
-  title: 'pages/Button',
+  title: 'common/Button',
   component: Button,
-  parameters: {},
+  argTypes: {
+    children: {
+      defaultValue: '로그인 또는 회원가입',
+    },
+    size: {
+      control: {
+        type: 'select',
+        options: ['large', 'big', 'normal', 'small'],
+      },
+      defaultValue: 'large',
+    },
+    design: {
+      control: {
+        type: 'select',
+        options: ['solid', 'ghost', 'outline', 'reject'],
+      },
+    },
+    backgroundColor: {
+      control: {
+        type: 'select',
+        options: ['#5344AA', '#E14D6C', '#FFF', '#FEE500'],
+      },
+    },
+    color: {
+      control: {
+        type: 'select',
+        options: ['#FFF', '#000', '#5344AA'],
+      },
+    },
+  },
 } as ComponentMeta<typeof Button>;
 
-const Template: ComponentStory<typeof Button> = () => <Button>Button!</Button>;
+const Template: ComponentStory<typeof Button> = () => (
+  <Button
+    design="reject"
+    width={347}
+    size="large"
+    color="#FFF"
+    backgroundColor="#5344AA"
+  >
+    로그인 또는 회원가입
+  </Button>
+);
 
 export const Default = Template.bind({});
+
+export const LargeButton = () => (
+  <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+    <Button size="large" width={347} color="#FFF" backgroundColor="#5344AA">
+      로그인 또는 회원가입
+    </Button>
+    <Button size="large" width={347} color="#FFF" backgroundColor="#E14D6C">
+      로그인 없이 시작하기
+    </Button>
+    <Button size="large" width={347} color="#000" backgroundColor="#FFF">
+      공유하기
+    </Button>
+  </div>
+);
+
+export const PopupButton = () => (
+  <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+    <Button size="large" width={140} color="#FFF" backgroundColor="#5344AA">
+      마저 할게요!
+    </Button>
+    <Button size="large" width={140} color="#000" backgroundColor="#FFF">
+      네
+    </Button>
+  </div>
+);
--- src/components/common/Button/Button.style.ts
+++ src/components/common/Button/Button.style.ts
@@ -1,18 +1,78 @@
+import { ReactNode } from 'react';
+
+import { flexbox } from '@/styles/mixin';
+
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
-export const Button = styled.button`
-  ${() => css`
-    padding: 8px 16px;
-    background-color: #007bff;
-    color: #fff;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    transition: background-color 0.3s ease;
+type ButtonSizeType = 'large' | 'big' | 'normal' | 'small';
+type DesignType = 'solid' | 'ghost' | 'outline' | 'reject';
 
-    &:hover {
-      background-color: #0056b3;
+export interface ButtonProps {
+  children?: ReactNode;
+  design?: DesignType;
+  width: number;
+  size: ButtonSizeType;
+  backgroundColor?: string;
+  color?: string;
+}
+
+const padding: Record<ButtonSizeType, string> = {
+  large: '0 16px',
+  big: '0 16px',
+  normal: '0 12px',
+  small: '0 12px',
+};
+
+const height: Record<ButtonSizeType, number> = {
+  large: 58,
+  big: 50,
+  normal: 42,
+  small: 34,
+};
+
+// ButtonBase 만든이유 ? 기존 default Button 이외에 다른 종류의 버튼이 생길 수도 있어서(Ex. ToggleButton)
+const ButtonBase = styled.button`
+  ${flexbox({ jc: 'center', ai: 'center' })};
+  appearance: none;
+  margin: 0;
+  border: none;
+  border-radius: 0;
+  padding: 0;
+  outline: none;
+  cursor: pointer;
+  box-sizing: border-box;
+
+  text-align: initial;
+  font-family: Roboto;
+  background-color: transparent;
+  color: #000;
+
+  &:disabled {
+    cursor: default;
+  }
+`;
+
+export const Button = styled(ButtonBase)<ButtonProps>`
+  color: ${(props) => props.color};
+  background-color: ${(props) => props.backgroundColor};
+  padding: ${({ size }) => padding[size]}px;
+  width: ${({ width }) => width}px;
+  height: ${({ size }) => height[size]}px;
+  border-radius: 13px;
+
+  ${() => css`
+    &.reject {
+      border: 1px solid #ff5757;
+      color: #ff5757;
     }
-  `}
+    &.reject:hover {
+      background: ##FFE5E5
+    }
+    &.reject:disabled,
+    &.reject.disabled {
+      background: ##FFE5E5
+      color: #ff5757;
+    }
+  `};
 `;
