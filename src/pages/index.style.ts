a3bd02a3adb1a9ffd3ddd0d7d6e43361e8949d43
sungjin.kim@voithru.com <sungjin.kim@voithru.com>
2023-07-02 12:12:45 +0900

    🚧: Main Page UI Complete.

---
+++ src/pages/index.style.ts
@@ -0,0 +1,43 @@
+import { flexbox } from '@/styles/mixin';
+
+import styled from '@emotion/styled';
+
+export const MainWrapper = styled.section`
+  ${flexbox({ dir: 'column', ai: 'center' })};
+  width: 100%;
+  height: 100%;
+  padding: 134px 0 78px 0;
+`;
+
+export const Title = styled.h1`
+  color: #5344aa;
+  font-size: 22px;
+  font-family: NanumSquare Neo;
+  font-weight: 400;
+`;
+
+export const Text = styled.span`
+  font-size: 18px;
+  font-family: Roboto;
+  font-weight: 500;
+  line-height: 22px;
+  letter-spacing: 1.25px;
+`;
+
+export const Logo = styled.img`
+  width: 240px;
+  height: 140px;
+  margin-top: 50px;
+`;
+
+export const Description = styled.span``;
+
+export const LoginButtonWrapper = styled.div`
+  margin-top: 27px;
+`;
+
+export const BottomWrapper = styled.div`
+  ${flexbox({ dir: 'column', jc: 'flex-end', ai: 'center' })};
+  margin-top: 120px;
+  gap: 16px;
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,54 @@
-import Button from '@/components/common/Button';
+import { useRouter } from 'next/router';
 
-function Home() {
+import Button from '@/components/common/Button';
+import theme from '@/styles/theme';
+
+import { css } from '@emotion/react';
+
+import * as S from './index.style';
+
+function Main() {
+  const router = useRouter();
   return (
-    <div>
-      Home Page.
-      <Button
-        width={397}
-        size="large"
-        design="reject"
-        backgroundColor="#5344AA"
-        color="#fff"
-      >
-        Test Button
-      </Button>
-    </div>
+    <S.MainWrapper>
+      <S.Title>여행 스타일 테스트</S.Title>
+      <S.Logo src="/images/logo.png" alt="logo image" />
+      <S.Description>성향에 딱 맞는 여행지를 알려드려요!</S.Description>
+      <S.LoginButtonWrapper>
+        <Button
+          width={333}
+          size="large"
+          backgroundColor={theme.color.primary}
+          color="#fff"
+          onClick={() => router.push('/login')}
+        >
+          <S.Text>로그인 또는 회원가입</S.Text>
+        </Button>
+      </S.LoginButtonWrapper>
+      <S.BottomWrapper>
+        <Button
+          width={333}
+          size="large"
+          backgroundColor={theme.color.secondary}
+          color="#fff"
+          onClick={() => router.push('/test/1')}
+        >
+          <S.Text>로그인 없이 시작하기</S.Text>
+        </Button>
+        <Button
+          width={333}
+          size="large"
+          backgroundColor="#fff"
+          color="##585858"
+          css={css`
+            border: 1px solid black;
+          `}
+        >
+          <S.Text>공유하기</S.Text>
+        </Button>
+      </S.BottomWrapper>
+    </S.MainWrapper>
   );
 }
 
-export default Home;
+export default Main;
