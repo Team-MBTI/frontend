f36e8ae68b1db093f8a518592b9f9431cb49f4d9
JeongMin Lee <clcl6084@gmail.com>
2023-07-04 00:57:56 +0900

    ♻️: apply common component

--- src/pages/test/[id]/index.tsx
+++ src/pages/test/[id]/index.tsx
@@ -3,20 +3,12 @@ import { RefObject, useRef } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
+import Button from '@/components/common/Button';
 import ProgressBar from '@/components/common/ProgressBar';
 import * as S from '@/components/test/index.style';
 import { useMbtiTest } from '@/hooks/useMBTITest';
 import { useToastMessage } from '@/store/GlobalStore';
 import theme from '@/styles/theme';
-
-import styled from '@emotion/styled';
-
-const Layout = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-`;
 
 interface IMbtiTest {
   question: string;
@@ -83,70 +75,71 @@ function Index() {
   const setToastMessage = useToastMessage((state) => state.setToastMessage);
 
   return (
-    <Layout>
-      <S.Section>
-        <Link href="/">
-          <Image
-            priority
-            src="/svg/back.svg"
-            height={44}
-            width={44}
-            alt="back"
-          />
-        </Link>
-        {mbtiTest.map((test, index) => (
-          <S.Question
-            key={test.question}
-            isActive={currentStep === index}
-            ref={(element: HTMLDivElement | null) => {
-              if (element && questionRef.current) {
-                questionRef.current[index] = element;
-              }
-            }}
-          >
-            <p>{test.question}</p>
-            <div>
-              {test.answer.map((answer) => (
-                <S.Card
-                  isActive={currentStep === index}
-                  key={test.question + answer}
-                  type="button"
-                  onClick={() => {
-                    if (currentStep - 1 === index) {
-                      moveToNextQuestion(index);
-                    }
-                  }}
-                  onAnimationEnd={() => {
-                    if (currentStep === index) {
-                      moveToNextQuestion(index + 1);
-                    }
-                  }}
-                >
-                  <p>선택지에 대한 설명</p>
-                </S.Card>
-              ))}
-            </div>
-          </S.Question>
-        ))}
-        <ProgressBar currentStep={currentStep} color={theme.color.primary} />
-        {currentStep > 0 && (
-          <S.PreviousArea
-            onClick={() => {
-              moveToNextQuestion(currentStep - 1);
-            }}
-          />
-        )}
-        <S.ResultButton
+    <S.Section>
+      <Link href="/">
+        <Image priority src="/svg/back.svg" height={44} width={44} alt="back" />
+      </Link>
+      {mbtiTest.map((test, index) => (
+        <S.Question
+          key={test.question}
+          isActive={currentStep === index}
+          ref={(element: HTMLDivElement | null) => {
+            if (element && questionRef.current) {
+              questionRef.current[index] = element;
+            }
+          }}
+        >
+          <p>{test.question}</p>
+          <div>
+            {test.answer.map((answer) => (
+              <S.Card
+                isActive={currentStep === index}
+                key={test.question + answer}
+                type="button"
+                onClick={() => {
+                  if (currentStep - 1 === index) {
+                    moveToNextQuestion(index);
+                  }
+                }}
+                onAnimationEnd={() => {
+                  if (currentStep === index) {
+                    moveToNextQuestion(index + 1);
+                  }
+                }}
+              >
+                <p>선택지에 대한 설명</p>
+              </S.Card>
+            ))}
+          </div>
+        </S.Question>
+      ))}
+      <ProgressBar currentStep={currentStep} color={theme.color.primary} />
+      {currentStep > 0 && (
+        <S.PreviousArea
+          onClick={() => {
+            moveToNextQuestion(currentStep - 1);
+          }}
+        />
+      )}
+      {currentStep !== 12 && <S.NextArea />}
+      <div style={{ marginBottom: '10px' }}>
+        <Button
+          width={320}
+          design="solid"
+          size="big"
+          backgroundColor={theme.color.primary}
+          color="white"
           onClick={() =>
             setToastMessage({
               message: '거의 다 왔어요!',
               type: 'success',
             })}
+          disabled={currentStep !== 12}
         >
           결과 확인 하기
-        </S.ResultButton>
-      </S.Section>
-    </Layout>
+        </Button>
+      </div>
+    </S.Section>
   );
 }
 
